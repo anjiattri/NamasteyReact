f3285f90b394b2107facf9b7cdad03693363d619
anji.attri <attri2707@gmal.com>
2023-08-24 22:33:52 +0530

    changes

--- EP-3(FOOD APP)/src/components/AboutUs.js
+++ EP-3(FOOD APP)/src/components/AboutUs.js
@@ -1,7 +1,15 @@
 import React from "react";
+import User from "./User";
+import UserClass from "./UserClass";
 
 function AboutUs() {
-  return <div>AboutUs</div>;
+  return (
+    <div>
+      <h1>About Us</h1>
+      <User name="Anjali Function" />
+      <UserClass name="Anjali Class" />
+    </div>
+  );
 }
 
 export default AboutUs;
