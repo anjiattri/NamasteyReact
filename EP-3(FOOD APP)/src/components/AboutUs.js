01bad205cfeaf807fc328014f6a4ce057b7f345b
anji.attri <attri2707@gmail.com>
2023-08-30 14:12:34 +0530

    class component

--- EP-3(FOOD APP)/src/components/AboutUs.js
+++ EP-3(FOOD APP)/src/components/AboutUs.js
@@ -1,15 +1,35 @@
 import React from "react";
 import User from "./User";
-import UserClass from "./UserClass";
 
-function AboutUs() {
-  return (
-    <div>
-      <h1>About Us</h1>
-      <User name="Anjali Function" />
-      <UserClass name="Anjali Class" />
-    </div>
-  );
+// function AboutUs() {
+//   return (
+//     <div>
+//       <h1>About Us</h1>
+//       <User name="Anjali Function" location="delhi" />
+//     </div>
+//   );
+// }
+
+// export default AboutUs;
+
+class AboutUs extends React.Component {
+  constructor(props) {
+    super(props);
+    console.log("parent constructor");
+  }
+  componentDidMount() {
+    console.log("parent componentDidMount");
+  }
+  render() {
+    console.log("parent render");
+
+    return (
+      <div>
+        <h1>About Us</h1>
+        <User name="Avi " location="bangore" />
+        <User name="Anjali " location="delhi" />
+      </div>
+    );
+  }
 }
-
 export default AboutUs;
--- EP-3(FOOD APP)/src/components/User.js
+++ EP-3(FOOD APP)/src/components/User.js
@@ -1,9 +1,36 @@
-const User = (props) => {
-  return (
-    <div className="user-card">
-      <h2>Name : {props.name}</h2>
-      <h3>Location: Delhi</h3>
-    </div>
-  );
-};
+import React from "react";
+class User extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      count: 0,
+    };
+    console.log(this.props.name, "child constructor");
+  }
+
+  componentDidMount() {
+    console.log(this.props.name, "child componentDidMount");
+  }
+  render() {
+    console.log(this.props.name, "child render");
+
+    const { name, location } = this.props;
+    const { count } = this.state;
+
+    return (
+      <div className="user-card">
+        <h2>Name : {name}</h2>
+        <h3>Location: {location}</h3>
+        <h3>Count :{count}</h3>
+        <button
+          onClick={() => {
+            this.setState({ count: this.state.count + 1 });
+          }}
+        >
+          Increment
+        </button>
+      </div>
+    );
+  }
+}
 export default User;
