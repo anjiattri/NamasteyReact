cff41fb94f976833191681710102df1ba7ec011b
anji.attri <attri2707@gmail.com>
2023-09-11 16:42:09 +0530

    responsive

--- netflix-gpt/src/components/Header.js
+++ netflix-gpt/src/components/Header.js
@@ -65,7 +65,7 @@ const Header = () => {
     <div className="absolute w-full  px-8 py-2 bg-gradient-to-b from-black z-10 flex flex-col justify-between md:flex-row">
       <img className="w-44 mx-auto md:mx-0" src={LOGO_URL} alt="logo" />
       {user && (
-        <div className="flex p-2">
+        <div className="flex p-2 justify-between">
           {showGPTSearch && (
             <select
               className="py-2 px-4 m-2 bg-gray-800 text-white"
@@ -85,7 +85,7 @@ const Header = () => {
           >
             {showGPTSearch ? "Home" : "GPT Search"}
           </button>
-          <img className="w-14 h-14 " src={user?.photoURL} alt="user" />
+          <img className="w-14 h-14 hidden md:block" src={user?.photoURL} alt="user" />
           <button
             className=" rounded-lg text-white font-bold m-4"
             onClick={handleLogout}
